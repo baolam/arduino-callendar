e21d6211308f16328bffe62d287f524478ff6f51
baolam <63187255+baolam@users.noreply.github.com>
2020-11-20 16:30:56 +0700

    Update SmartClass.ino

--- SmartClass.ino
+++ SmartClass.ino
@@ -1,65 +1,22 @@
-#include <SPI.h>
-#include <Wire.h>
-#include <SD.h>
-#include <Ethernet.h>
-#include <ArduinoHttpClient.h>
-#include <ArduinoJson.h>
-#include <DHT.h>
-
-#define DEBUG
 #include "Callendar.h"
-
-#define CHIP_SELECT 4
-#define ADDRESS_3231 0x27
-#define ISD1820 5
-#define DHTPIN 7
-#define DHTTYPE 11
-
-byte mac[] = {};
-const char* host = "";
-const int port = 80;
-const char* appurl = "";
-const char* appjson = "";
-
-IPAddress ip(192, 168, 1, 20);
-IPAddress gateway(192, 168, 1, 1);
-IPAddress subnet(255, 255, 255, 0);
 Callendar callendar;
-EthernetClient client;
-HttpClient http(client, host, port);
-DHT dht(DHTPIN, DHTTYPE);
-File file;
 
-void readJson(const char* url);
-int convertBintoDec(String data);
+void inSerial();
 
 void setup() {
   // put your setup code here, to run once:
-  if(! SD.begin(CHIP_SELECT)) {
-    
-  }
+  callendar.begin(9600, false);
+  Serial.begin(9600);
+  Serial.println("Testing...");
+  callendar.add("15:00:00", "16:30:00", inSerial);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  callendar.loop();
+  delay(5000);
 }
 
-void readJson(const char* url) {
-  file = SD.open(url);
-  String dt;
-  if(file) {
-    if(file.available()) {
-      dt = file.read();
-    }
-  }  
-
-  DynamicJsonDocument doc(1024);
-  DeserializationError error = deserializeJson(doc, dt);
-
-  if(error) {
-    return;
-  }
-
-  JsonObject obj = doc.as<JsonObject>();
+void inSerial() {
+  Serial.println("Dang trong ke hoach...");
 }
